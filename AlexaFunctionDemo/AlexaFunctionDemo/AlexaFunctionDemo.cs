41d0a50e2354160f0af98b59d66bda6097cfc683
Miguel Teheran <miguel.teheran@gorillalogic.com>
2019-07-11 12:45:41 -0500

    implementing a refactoring in the code

--- AlexaFunctionDemo/AlexaFunctionDemo/AlexaFunctionDemo.cs
+++ AlexaFunctionDemo/AlexaFunctionDemo/AlexaFunctionDemo.cs
@@ -29,82 +29,79 @@ namespace AlexaFunctionDemo
             Session session = skillRequest.Session;
 
             SkillResponse response = null;
+            response.Response.ShouldEndSession = false;
 
             if (requestType == typeof(LaunchRequest))
             {
                 response = ResponseBuilder.Tell("Welcome to Gorilla Logic!");
-                response.Response.ShouldEndSession = false;
             }
             else if (requestType == typeof(IntentRequest))
             {
                 var intentRequest = skillRequest.Request as IntentRequest;
 
-                if (intentRequest.Intent.Name.ToLower() == "gorillalocation")
+                switch (intentRequest.Intent.Name.ToLower())
                 {
-                    response = ResponseBuilder.Tell("Gorilla Logic is located in Ruta N medellin Colombia oficina 2020");
-                    var speech = new SsmlOutputSpeech();
-                    speech.Ssml = "<speak>Gorilla Logic is located in <lang xml:lang=\"es-ES\">Ruta Ene Medellin Colombia oficina 2020</lang></speak>";
-                    //response = ResponseBuilder.Tell(speech);
-                    response.Response.ShouldEndSession = false;
-                }
-                else if (intentRequest.Intent.Name.ToLower() == "gorillamusic" || intentRequest.Intent.Name == "AMAZON.ResumeIntent")
-                {
-                    string audioUrl = "https://audiodemosmp3.s3.amazonaws.com/Gorillaz_-_19-2000_lyrics.mp3";
-                    string audioToken = "Gorillaz song 19 - 20000";
-                    var speech = new SsmlOutputSpeech();
-                    speech.Ssml = $"<speak>{audioToken}<audio src=\"{audioUrl}\"/></speak>";
-                    //response = ResponseBuilder.Tell(speech);
-                    response = ResponseBuilder.AudioPlayerPlay(PlayBehavior.ReplaceAll, audioUrl, audioToken, (int)skillRequest.Context.AudioPlayer.OffsetInMilliseconds);
-                    response.Response.ShouldEndSession = false;
-
-                }
-                else if (intentRequest.Intent.Name.ToLower() == "gorillainvitation")
-                {
-                    var speech = new SsmlOutputSpeech();
-                    speech.Ssml = "<speak><lang xml:lang=\"es-ES\"><voice name=\"Enrique\">Estan todos invitados al meetup del 25 de julio donde yo alexa seré la protagonista. Los esperamos en Ruta N</voice></lang></speak>";
-                    response = ResponseBuilder.Tell(speech);
-                    response.Response.ShouldEndSession = true;
-                }
-                else if (intentRequest.Intent.Name.ToLower() == "gorillacalculation")
-                {
-                    Reprompt reprompt = new Reprompt("you said {year}");
-                    if (intentRequest.Intent.Slots.Count > 0)
-                    {
-                        if (intentRequest.Intent.Slots["year"] != null &&
-                            intentRequest.Intent.Slots["year"].Value!=null &&
-                            intentRequest.Intent.Slots["date"] != null &&
-                            intentRequest.Intent.Slots["date"].Value != null)
+                    case "gorillalocation":
+                        response = ResponseBuilder.Tell("Gorilla Logic is located in Ruta N medellin Colombia oficina 2020");
+                        var speech = new SsmlOutputSpeech();
+                        speech.Ssml = "<speak>Gorilla Logic is located in <lang xml:lang=\"es-ES\">Ruta Ene Medellin Colombia oficina 2020</lang></speak>";
+                        //response = ResponseBuilder.Tell(speech);
+                        break;
+                    case "gorillamusic":
+                    case "AMAZON.ResumeIntent":
+                        string audioUrl = "https://audiodemosmp3.s3.amazonaws.com/Gorillaz_-_19-2000_lyrics.mp3";
+                        string audioToken = "Gorillaz song 19 - 20000";
+                        var speechMusic = new SsmlOutputSpeech();
+                        //speech.Ssml = $"<speak>{audioToken}<audio src=\"{audioUrl}\"/></speak>";
+                        //response = ResponseBuilder.Tell(speechMusic);
+                        response = ResponseBuilder.AudioPlayerPlay(PlayBehavior.ReplaceAll, audioUrl, audioToken, (int)skillRequest.Context.AudioPlayer.OffsetInMilliseconds);
+                        break;
+                    case "gorillainvitation":
+                        var speechInivtation = new SsmlOutputSpeech();
+                        speechInivtation.Ssml = "<speak><lang xml:lang=\"es-ES\"><voice name=\"Enrique\">Estan todos invitados al meetup del 25 de julio donde yo alexa seré la protagonista. Los esperamos en Ruta N</voice></lang></speak>";
+                        response = ResponseBuilder.Tell(speechInivtation);
+                        break;
+                    case "gorillacalculation":
+                        Reprompt reprompt = new Reprompt("you said {year}");
+                        if (intentRequest.Intent.Slots.Count > 0)
                         {
-                            DateTime dateValue = DateTime.Parse(intentRequest.Intent.Slots["date"].Value.ToString());
+                            if (intentRequest.Intent.Slots["year"] != null &&
+                                intentRequest.Intent.Slots["year"].Value != null &&
+                                intentRequest.Intent.Slots["date"] != null &&
+                                intentRequest.Intent.Slots["date"].Value != null)
+                            {
+                                DateTime dateValue = DateTime.Parse(intentRequest.Intent.Slots["date"].Value.ToString());
 
-                            dateValue.AddYears(int.Parse(intentRequest.Intent.Slots["year"].Value.ToString()));
+                                dateValue.AddYears(int.Parse(intentRequest.Intent.Slots["year"].Value.ToString()));
 
-                            int result = (DateTime.Now - dateValue).Days/365;
+                                int result = (DateTime.Now - dateValue).Days / 365;
 
-                            response = ResponseBuilder.Tell($"you are {result} years old");
-                            response.Response.ShouldEndSession = true;
+                                response = ResponseBuilder.Tell($"you are {result} years old");
+                                response.Response.ShouldEndSession = true;
 
+                            }
+                            else
+                            {
+                                response = ResponseBuilder.Ask("Please tell me, when were you born?", reprompt);
+                            }
                         }
                         else
                         {
                             response = ResponseBuilder.Ask("Please tell me, when were you born?", reprompt);
                         }
-                    }
-                    else
-                    {
-                        response = ResponseBuilder.Ask("Please tell me, when were you born?", reprompt);
-                    }
+                        break;
+                    case "amazon.pauseintent":
+                        response = ResponseBuilder.AudioPlayerStop();
+                        break;
+                    default:
+                        break;
                 }
-                else if (intentRequest.Intent.Name == "AMAZON.PauseIntent")
-                {
-                    response = ResponseBuilder.AudioPlayerStop();
-                    response.Response.ShouldEndSession = false;
-                }
+
             }
             else if (requestType == typeof(SessionEndedRequest))
             {
                 response = ResponseBuilder.Tell("bye");
-                response.Response.ShouldEndSession = false;
+                response.Response.ShouldEndSession = true;
             }
             else if (requestType == typeof(AudioPlayerRequest))
             {
@@ -112,17 +109,10 @@ namespace AlexaFunctionDemo
                 var audioRequest = skillRequest.Request as AudioPlayerRequest;
 
                 //
-                if (audioRequest.AudioRequestType == AudioRequestType.PlaybackStopped)
-                {
-
-
-                }
+                //if (audioRequest.AudioRequestType == AudioRequestType.PlaybackStopped)
 
                 //
-                if (audioRequest.AudioRequestType == AudioRequestType.PlaybackNearlyFinished)
-                {
-
-                }
+                //if (audioRequest.AudioRequestType == AudioRequestType.PlaybackNearlyFinished)
             }
             else
             {

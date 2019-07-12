486dffb7cb68533cb8c7441e4e454c183b1e6a5a
Miguel Teheran <miguel.teheran@gorillalogic.com>
2019-07-12 11:15:47 -0500

    Fixing las code and adding last refactoring

--- AlexaFunctionDemo/AlexaFunctionDemo/AlexaFunctionDemo.cs
+++ AlexaFunctionDemo/AlexaFunctionDemo/AlexaFunctionDemo.cs
@@ -29,11 +29,12 @@ namespace AlexaFunctionDemo
             Session session = skillRequest.Session;
 
             SkillResponse response = null;
-            response.Response.ShouldEndSession = false;
 
             if (requestType == typeof(LaunchRequest))
             {
                 response = ResponseBuilder.Tell("Welcome to Gorilla Logic!");
+                response.Response.ShouldEndSession = false;
+
             }
             else if (requestType == typeof(IntentRequest))
             {
@@ -44,11 +45,11 @@ namespace AlexaFunctionDemo
                     case "gorillalocation":
                         response = ResponseBuilder.Tell("Gorilla Logic is located in Ruta N medellin Colombia oficina 2020");
                         var speech = new SsmlOutputSpeech();
-                        speech.Ssml = "<speak>Gorilla Logic is located in <lang xml:lang=\"es-ES\">Ruta Ene Medellin Colombia oficina 2020</lang></speak>";
-                        //response = ResponseBuilder.Tell(speech);
+                        //speech.Ssml = "<speak>Gorilla Logic is located in <lang xml:lang=\"es-ES\">Ruta Ene Medellin Colombia oficina 2020</lang></speak>";
+                        response = ResponseBuilder.Tell(speech);
                         break;
                     case "gorillamusic":
-                    case "AMAZON.ResumeIntent":
+                    case "amazon.resumeintent":
                         string audioUrl = "https://audiodemosmp3.s3.amazonaws.com/Gorillaz_-_19-2000_lyrics.mp3";
                         string audioToken = "Gorillaz song 19 - 20000";
                         var speechMusic = new SsmlOutputSpeech();
@@ -57,12 +58,11 @@ namespace AlexaFunctionDemo
                         response = ResponseBuilder.AudioPlayerPlay(PlayBehavior.ReplaceAll, audioUrl, audioToken, (int)skillRequest.Context.AudioPlayer.OffsetInMilliseconds);
                         break;
                     case "gorillainvitation":
-                        var speechInivtation = new SsmlOutputSpeech();
-                        speechInivtation.Ssml = "<speak><lang xml:lang=\"es-ES\"><voice name=\"Enrique\">Estan todos invitados al meetup del 25 de julio donde yo alexa seré la protagonista. Los esperamos en Ruta N</voice></lang></speak>";
-                        response = ResponseBuilder.Tell(speechInivtation);
+                        var speechInvitation = new SsmlOutputSpeech();
+                        speechInvitation.Ssml = "<speak><voice name=\"Enrique\"><lang xml:lang=\"es-ES\">Estan todos invitados al meetup del 25 de julio donde yo alexa seré la protagonista. Los esperamos en Ruta N</lang></voice></speak>";
+                        response = ResponseBuilder.Tell(speechInvitation);
                         break;
                     case "gorillacalculation":
-                        Reprompt reprompt = new Reprompt("you said {year}");
                         if (intentRequest.Intent.Slots.Count > 0)
                         {
                             if (intentRequest.Intent.Slots["year"] != null &&
@@ -72,7 +72,7 @@ namespace AlexaFunctionDemo
                             {
                                 DateTime dateValue = DateTime.Parse(intentRequest.Intent.Slots["date"].Value.ToString());
 
-                                dateValue.AddYears(int.Parse(intentRequest.Intent.Slots["year"].Value.ToString()));
+                                dateValue = dateValue.AddYears(int.Parse(intentRequest.Intent.Slots["year"].Value.ToString()) - dateValue.Year);
 
                                 int result = (DateTime.Now - dateValue).Days / 365;
 
@@ -82,12 +82,12 @@ namespace AlexaFunctionDemo
                             }
                             else
                             {
-                                response = ResponseBuilder.Ask("Please tell me, when were you born?", reprompt);
+                                response = ResponseBuilder.Ask("Please tell me, when were you born?", null);
                             }
                         }
                         else
                         {
-                            response = ResponseBuilder.Ask("Please tell me, when were you born?", reprompt);
+                            response = ResponseBuilder.Ask("Please tell me, when were you born?", null);
                         }
                         break;
                     case "amazon.pauseintent":
